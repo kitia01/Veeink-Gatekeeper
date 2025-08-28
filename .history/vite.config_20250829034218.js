b15412222280c6583b36edf9cec1b856d7fb53d7
wlf <fwl7747@126.com>
2025-08-29 05:58:07 +0800

    第一版最终决定

--- .history/vite.config_20250829034218.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: './',  // 确保相对路径，部署到子路径也可用
-  root: './',  // 指定根目录
-})
--- .history/vite.config_20250829041353.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
-export default defineConfig({
-  plugins: [vue()],
-  base: '/', // 根路径，确保部署后资源路径正确
-});
